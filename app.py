5d1bdb520ab495b790c870f849973f15113c40fa
tinyautimothyyu <tinyau.yu@alumni.ubc.ca>
2023-08-03 17:20:46 -0700

    updated app and installed pandas

--- app.py
+++ app.py
@@ -1,29 +1,50 @@
 import requests
 import json
+from dotenv import load_dotenv
+import os
+import pandas as pd
+
+load_dotenv()
+
+access_token = os.getenv('ACCESS_TOKEN')
+phone_number_id = os.getenv('PHONE_NUMBER_ID')
+
+if not access_token:
+    raise ValueError("Access token not found. Please check your .env file.")
+if not phone_number_id:
+    raise ValueError("Phone number ID not found. Please check your .env file.")
+
+def get_contacts_from_csv(path):
+    df = pd.read_csv(path)
+    pass
+
+def get_pass(contact):
+    pass
 
 # Your API URL
-url = "https://graph.facebook.com/v17.0/110567488787341/messages"
+url = f"https://graph.facebook.com/v17.0/{phone_number_id}/messages"
 
 headers = {
-    "Authorization": "Bearer EAAJXl7sAQcoBO40viApDACAYhBxjIZA90BK4lzPk2xYD6jnuwMgRnwVYM0ICJYpcSLFDh2haeGKg9ZC900Tau1OdX3MAs1sNKl9HijHnKw4X432bfoT9DoDUdQQqidXfZBbJiKokhlueK9pvksMHpYqYXRcZA1ZCvk7ZBiMDbZAsurAR1Vk27LZAVEsJN7oBPrMUH1qqOJvpzNfhCG3rZCz1iCidnvuSH8WDBT8xnXgZDZD",
+    "Authorization": "Bearer "+access_token,
     "Content-Type": "application/json"
 }
 
-# First you need to base64 encode your image
-# Define the message content
-body = { "messaging_product": "whatsapp", 
-        "to": "16045377369", 
-        "type": "image",
-        "image": {
-            "link": "https://drive.google.com/file/d/1QkxHNlDt3AyWKbnhb1dkw9jdI8kmPc1D/view?usp=sharing"
-        }
-        }
+def send_message(url, headers, contact_number, image_link):
+    # First you need to base64 encode your image
+    # Define the message content
+    body = { "messaging_product": "whatsapp", 
+            "to": contact_number, 
+            "type": "image",
+            "image": {
+                "link": image_link
+            }
+            }
 
-# Send the POST request
-response = requests.post(url, headers=headers, data=json.dumps(body))
+    # Send the POST request
+    response = requests.post(url, headers=headers, data=json.dumps(body))
 
-# Print the response
-if response.status_code == 200:
-    print("Message sent successfully")
-else:
-    print(f"Failed to send message. Status code: {response.status_code}, Response: {response.text}")
+    # Print the response
+    if response.status_code == 200:
+        print(f"Message sent successfully to {contact_number}")
+    else:
+        print(f"Failed to send message. Status code: {response.status_code}, Response: {response.text}")
