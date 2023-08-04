536d8e31bc451f1163983f97c45c948a77d2aaa8
tinyautimothyyu <tinyau.yu@alumni.ubc.ca>
2023-08-04 09:36:17 -0700

    organized codes to be more oop

--- app.py
+++ app.py
@@ -1,50 +1,33 @@
-import requests
-import json
+import argparse
 from dotenv import load_dotenv
 import os
-import pandas as pd
+from helpers import Contacts, Messaging
 
+# Argument parsing
+parser = argparse.ArgumentParser(description="Send AYAYA pass to contacts from a CSV file.")
+parser.add_argument('csv_path', type=str, help="Path to the CSV file containing contacts.")
+args = parser.parse_args()
+csv_path = args.csv_path
+
+# Load environment variables
 load_dotenv()
 
-access_token = os.getenv('ACCESS_TOKEN')
-phone_number_id = os.getenv('PHONE_NUMBER_ID')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+PHONE_NUMBER_ID = os.getenv('PHONE_NUMBER_ID')
 
-if not access_token:
+if not ACCESS_TOKEN:
     raise ValueError("Access token not found. Please check your .env file.")
-if not phone_number_id:
+if not PHONE_NUMBER_ID:
     raise ValueError("Phone number ID not found. Please check your .env file.")
 
-def get_contacts_from_csv(path):
-    df = pd.read_csv(path)
-    pass
+# Create instances of Contacts and Messaging classes
+contacts = Contacts(csv_path)
+messaging = Messaging(PHONE_NUMBER_ID, ACCESS_TOKEN)
 
-def get_pass(contact):
-    pass
+if not contacts:
+    raise ValueError("No contacts found in the CSV file.")
 
-# Your API URL
-url = f"https://graph.facebook.com/v17.0/{phone_number_id}/messages"
+phone_numbers = contacts.get_phone_numbers()
 
-headers = {
-    "Authorization": "Bearer "+access_token,
-    "Content-Type": "application/json"
-}
-
-def send_message(url, headers, contact_number, image_link):
-    # First you need to base64 encode your image
-    # Define the message content
-    body = { "messaging_product": "whatsapp", 
-            "to": contact_number, 
-            "type": "image",
-            "image": {
-                "link": image_link
-            }
-            }
-
-    # Send the POST request
-    response = requests.post(url, headers=headers, data=json.dumps(body))
-
-    # Print the response
-    if response.status_code == 200:
-        print(f"Message sent successfully to {contact_number}")
-    else:
-        print(f"Failed to send message. Status code: {response.status_code}, Response: {response.text}")
+for phone_number in phone_numbers:
+    messaging.send_message(phone_number)
--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pandas as pd
 
 class Media():
     def __init__(self):
@@ -24,10 +25,67 @@ class Media():
         # Print the response
         if response.status_code == 200:
             print(f"Media {file} uploaded successfully")
-            media_id = response.data.id  # assume the media_id is in data
+            media_id = response.json().get('id')
             self.media_id = media_id
         else:
-            print(f"Failed to send message. Status code: {response.status_code}, Response: {response.text}")
+            print(f"Failed to upload media. Status code: {response.status_code}, Response: {response.text}")
 
     def get_media_id(self):
         return self.media_id
+
+class Contacts():
+    def __init__(self, path):
+        self.df = pd.read_csv(path)
+
+    def get_phone_numbers(self):
+        # Assuming phone numbers are stored in a column named 'phone_number'
+        return self.df['phone_number'].tolist()
+
+class Messaging():
+    def __init__(self, PHONE_NUMBER_ID, ACCESS_TOKEN):
+        self.PHONE_NUMBER_ID = PHONE_NUMBER_ID
+        self.ACCESS_TOKEN = ACCESS_TOKEN
+
+    def _get_pass(self, PHONE_NUMBER):
+        pass
+
+    def send_message(self, PHONE_NUMBER):
+
+        # Your API URL
+        url = f"https://graph.facebook.com/v17.0/{self.PHONE_NUMBER_ID}/messages"
+
+        headers = {
+            "Authorization": "Bearer " + self.ACCESS_TOKEN,
+            "Content-Type": "application/json"
+        }
+
+        # get the file path of the AYAYA pass given the phone number
+        file = self._get_pass(PHONE_NUMBER)
+
+        # instatiate the media object
+        ayaya_pass = Media()
+        
+        # upload the document to WhatsApp media and obtain the media_id
+        ayaya_pass.upload_media(self.PHONE_NUMBER_ID, self.ACCESS_TOKEN, file)
+
+        # get media_id
+        media_id = ayaya_pass.get_media_id()
+
+        # Define the message content
+        body = { "messaging_product": "whatsapp",
+                "recipient_type": "individual", 
+                "to": PHONE_NUMBER, 
+                "type": "document",
+                "document": {
+                    "id": media_id
+                }
+                }
+
+        # Send the POST request
+        response = requests.post(url, headers=headers, data=json.dumps(body))
+
+        # Print the response
+        if response.status_code == 200:
+            print(f"Message sent successfully to {PHONE_NUMBER}")
+        else:
+            print(f"Failed to send message. Status code: {response.status_code}, Response: {response.text}")
